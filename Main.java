ca52917e2f1b1e4fa93c4f5946938603d26256cd
Etrella39 <ju020807@naver.com>
2024-05-08 23:02:52 +0900

    업데이트내용

--- Main.java
+++ Main.java
@@ -6,14 +6,6 @@ class Main {
         SwingUtilities.invokeLater(() -> {
             createAndShowGUI();
         });
-
-
-        String[] fontNames = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-
-        // 사용 가능한 모든 폰트 이름을 출력합니다.
-        for (String fontName : fontNames) {
-            System.out.println(fontName);
-        }
     }
 
     private static void createAndShowGUI() {
